6eb07bed3a8901f26d389535ea6474c85d7a07d3
Gabriella Barbosa <gabibsdev@gmail.com>
2023-07-18 23:38:43 -0300

    Inicia o header - ainda não responsivo

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        black: '#000000',
+        darkgrey: '#1D1D1D',
+        lightgrey: '#3A3A3A',
+        white: '#FFFFFF',
+      },
+      backgroundImage: {
+        'initial-content': "url('/assets/initial-conteiner')",
+      }
+    },
+    fontFamily: {
+      'poiret': ['Poiret One', 'sans-serif'],
+      'marcellus': ['Marcellus', 'serif'],
+    },
+  },
+}
+
